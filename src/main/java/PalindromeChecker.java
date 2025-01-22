a22f9c2e60e69501b84efab2a2bb875b9767d4f3
jnthnluong <jnthnluong@gmail.com>
2025-01-22 11:09:28 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -38,13 +38,17 @@ public void tester()
 public boolean palindrome(String word)
 {
   //your code here
-  if(false)return false;
+  if (word.equals(reverse(word))) return true;
   return false;
 }
 public String reverse(String str)
 {
-    String sNew = new String();
-    //your code here
-    return sNew;
+     String sNew = new String();
+  //your code here
+  for (int i = str.length()-1; i >= 0; i--) {
+    sNew = sNew+str.substring(i, i+1);
+  }
+  System.out.println("reversed:" + sNew);
+  return sNew;
 }
 }
